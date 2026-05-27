a14341bbeab9d8199b1a53d8cd54741b1ef0904a
Moe <aashiqgreen91@gmail.com>
2026-05-27 10:01:49 +0200

    Completed project requrements for Section B Question 1

--- SectionB-1/Program.cs
+++ SectionB-1/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿Console.Write("Welcome to the Structure of a C# Program Demo!\nEnter your name: ");
+string name = Console.ReadLine();
+
+Console.WriteLine($"\nHello, {name}!\n");
+
+Console.WriteLine("Program Structure Demonstrated:\n" +
+    "1. using System: imports functionality\n" +
+    "2. namespace: organized code\n" +
+    "3. class Program: container for code\n" +
+    "4. Main(): entry point of program\n" +
+    "5. Comments: explain logic and documentation\n\n" +
+    "Program executed successfully!");
